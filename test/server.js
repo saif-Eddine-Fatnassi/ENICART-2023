f3de9e02bfc6c0662f508ca4609fc8a0d3626e35
Saif <saiffedine.fatnassi@bial-r.com>
2023-07-05 17:04:30 +0200

    template

---
+++ test/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', async function (req, res) {
+    var myHeaders = new Headers();
+myHeaders.append("Authorization", "Basic aW5mcmFAYmlhbC14LmNvbTpUaWMmVGFjNjk=");
+myHeaders.append("Cookie", "JSESSIONID=73djxdztQXBV-HPov+GTVZoz; f_lb_t=https://10.0.6.68:2443");
+
+ 
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+const data = await fetch("https://bialx.fitnetmanager.com/FitnetManager/rest/contracts/read/1", requestOptions)
+  .then(response => response.text())
+  .catch(error => console.log('error', error));
+ return res.send(data);
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
---
+++ test/src/App.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { Button, Table } from "antd";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  useEffect(() => {
+    console.log("test");
+    fetch("/ping").then(() => console.log("data"));
+  }, []);
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <Button className="button" style={{ color: "red" }}>
+            button
+          </Button>
+          aEdit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <Table dataSource={dataSource} columns={columns} />;
+    </div>
+  );
+}
+
+export default App;
